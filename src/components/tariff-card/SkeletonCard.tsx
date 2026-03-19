6a9382e895b7a344c80da393bd79a1d997542a8e
Vladimir-Seropol <9906170@gmail.com>
2026-03-19 18:44:23 +0300

    edit SkeletonCard

--- src/components/tariff-card/SkeletonCard.tsx
+++ src/components/tariff-card/SkeletonCard.tsx
@@ -1,13 +1,31 @@
-export default function SkeletonCard() {
+export default function SkeletonCard({
+  className = "",
+}: {
+  className?: string;
+}) {
   return (
-    <div className="rounded-2xl border border-neutral-700 p-5 animate-pulse bg-[#1e2426]">
-      <div className="h-4 w-24 bg-neutral-700 rounded mb-3"></div>
+    <div
+      className={`
+        relative
+        w-full
+        min-h-[150px] xl:min-h-[240px]
+        rounded-2xl xl:rounded-[40px]
+        border-2 border-neutral-700
+        bg-neutral-800/50
+        overflow-hidden
+        ${className}
+      `}
+    >
+      <div className="absolute inset-0 bg-neutral-800/80" />
 
-      <div className="h-8 w-20 bg-neutral-700 rounded mb-2"></div>
+      <div className="absolute inset-0 -translate-x-full animate-shimmer bg-gradient-to-r from-transparent via-white/30 to-transparent" />
 
-      <div className="h-3 w-16 bg-neutral-700 rounded mb-3"></div>
-
-      <div className="h-3 w-full bg-neutral-700 rounded"></div>
+      <div className="relative opacity-0 p-5">
+        <div className="h-4 w-24 mb-3"></div>
+        <div className="h-8 w-20 mb-2"></div>
+        <div className="h-3 w-16 mb-3"></div>
+        <div className="h-3 w-full"></div>
+      </div>
     </div>
   );
 }
--- src/components/tariff/TariffSection.tsx
+++ src/components/tariff/TariffSection.tsx
@@ -27,48 +27,53 @@ export default function TariffSection({ seconds }: Props) {
   const lastThreeTariffs = sortedTariffs.slice(-3);
 
   return (
-    <main className="max-w-[1216px] w-full mx-auto px-4 xl:px-0 flex flex-col xl:flex-row items-center xl:gap-19 relative pt-[70px] xl:pt-[204px]">
-
+    <main className="max-w-[1216px] w-full mx-auto px-4 xl:px-0 flex flex-col xl:flex-row  xl:gap-19 relative pt-[70px] xl:pt-[204px]">
       <h1 className="absolute top-5 left-4 xl:left-0 xl:top-13 text-[22px] xs:text-[24px] xl:text-[40px] font-bold leading-[110%] pr-8">
         Выбери подходящий для себя{" "}
         <span className="text-card-accent">тариф</span>
       </h1>
 
-   
+      <div className=" flex-shrink-0 lg:flex xl:order-[-1] justify-center items-center mx-auto
+       mt-5 xl:mt-4 w-full max-w-[390px]">
+        <picture>
+          <source media="(min-width: 1280px)" srcSet="/man.png" />
 
-      <div className="flex-shrink-0 lg:flex xl:order-[-1] items-center justify-center mt-5 xl:mt-4 w-full max-w-[390px]">
-  <picture>
+          <source media="(min-width: 375px)" srcSet="/man375.png" />
 
-    <source
-      media="(min-width: 1280px)"
-      srcSet="/man.png"
-    />
+          <Image
+            src="/man320.png"
+            alt="man"
+            width={100}
+            height={200}
+            className="w-full max-w-[100px] xl:max-w-[390px] min-[375px]:max-w-[124px] h-auto object-contain mx-auto"
+            priority
+          />
+        </picture>
+      </div>
 
-    <source
-      media="(min-width: 375px)"
-      srcSet="/man375.png"
-    />
-
-    <Image
-      src="/man320.png"
-      alt="man"
-      width={100}
-      height={200}
-      className="w-full max-w-[100px] xl:max-w-[390px] min-[375px]:max-w-[124px] h-auto object-contain mx-auto"
-      priority
-    />
-  </picture>
-</div>
-
-      <div className="flex-1 max-w-[288px] xs:max-w-none">
+      <div className="flex-1  xs:max-w-none">
         {error && <div className="text-red-500 mb-4">{error}</div>}
 
         {loading ? (
-          <div className="grid gap-3 grid-cols-1 lg:grid-cols-3">
-            {Array.from({ length: 4 }).map((_, i) => (
-              <SkeletonCard key={i} />
-            ))}
-          </div>
+          <>
+            <div className="hidden xl:grid gap-3 grid-cols-1 lg:grid-cols-3 ">
+              {/* BEST */}
+              <SkeletonCard className="lg:col-span-3 xl:col-span-3 w-full xl:w-full" />
+
+              {/* остальные */}
+              <SkeletonCard />
+              <SkeletonCard />
+              <SkeletonCard />
+            </div>
+
+            {/* LAST THREE */}
+            <div className="w-full max-w-none grid gap-1 xs:gap-2 xl:gap-3 grid-cols-1 xl:grid-cols-3 mt-2 xl:mt-3 xl:hidden">
+              <SkeletonCard />
+              <SkeletonCard />
+              <SkeletonCard />
+              <SkeletonCard />
+            </div>
+          </>
         ) : (
           <>
             <TariffGrid
